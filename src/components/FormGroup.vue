713981f35dcddff1d6933544d3057c7c776e03db
Nate Stuyvesant <nstuyvesant@gmail.com>
2020-08-11 08:41:21 -0400

    chore(app): bump dependencies and adjust TypeScript to match new plugins

--- src/components/FormGroup.vue
+++ src/components/FormGroup.vue
@@ -6,26 +6,27 @@
 <script lang="ts">
 // Loosely based on Vue Formulate's FormulateForm https://vueformulate.com/guide/forms/#setting-initial-values
 // Wrapper for FormInputs so we can holistically track validation and prevent submits
-import { VNode } from "vue"
 import { defineComponent } from "@vue/composition-api"
 import { FormInputContext } from "@/use/form-input-validation"
 
 export default defineComponent({
   name: "FormGroup",
+  inheritAttrs: false,
   setup(_, { emit, slots }) {
     // TODO: Refactor validationErrorFree() and onFormSubmit() to composition function
+
     // Check validity of FormInputs in default slot
     const validationErrorFree = () => {
-      const vnodes: VNode[] = slots?.default()
-      const formInputs: VNode[] = vnodes?.filter(
-        (vnode) => vnode?.componentOptions?.tag === "FormInput",
+      const vnodes = slots.default()
+      const formInputs = vnodes.filter(
+        (vnode) => vnode.componentOptions?.tag === "FormInput",
       )
 
       // Loop through all elements tracking whether any are falsy
       // Can't use Array.prototype.every because it stops on first falsy
       let allFormInputsValid = true
       for (const vnode of formInputs) {
-        const formInput = vnode?.componentInstance as FormInputContext
+        const formInput = vnode.componentInstance as FormInputContext
         if (!formInput.valid) {
           if (allFormInputsValid) {
             formInput.focus()
